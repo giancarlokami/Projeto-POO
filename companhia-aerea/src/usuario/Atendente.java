cf107932c26d1ae5ff8b03b617d4ab5024cbeeb5
HACFelipe <felipehasc@gmail.com>
2019-07-31 18:43:02 -0300

    Implmentação do Swing

--- companhia-aerea/src/usuario/Atendente.java
+++ companhia-aerea/src/usuario/Atendente.java
@@ -3,8 +3,9 @@ package usuario;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.stream.Collectors;
+
+import javax.swing.JOptionPane;
 
 import aviao.Aviao;
 import aviao.Poltrona;
@@ -19,28 +20,27 @@ public class Atendente extends Usuario {
 	}
 	
 	public void geraRelatorio(ArrayList<Voo> voos){
-		Scanner sc = new Scanner(System.in);
+		String msg;
 		String escolha;
 		int id;
 		
 		if (voos.size() == 0) {
-			System.out.println("Nao ha voos cadastrados!");
-			sc.close();
+			msg = "Nao ha voos cadastrados!";
+			JOptionPane.showMessageDialog(null, msg, "Erro", JOptionPane.ERROR_MESSAGE);
 			return;
 		}
 		
 		do {
-			System.out.println("Escolha um voo para gerar um relatorio, ou 'S' para voltar: ");
+			msg = "Escolha um voo para gerar um relatorio, ou 'S' para voltar:\n";
 			for (Voo v: voos)
-				System.out.printf("| - %02d. Origem: %s | Destino: %s | Data: %td/%<tm/%<tY:%tH:%<tM\n"
+				msg += String.format("\n| - %02d. Origem: %s | Destino: %s | Data: %td/%<tm/%<tY:%tH:%<tM"
 								, voos.indexOf(v)
 								, v.getOrigem()
 								, v.getDestino()
 								, v.getData()
 								, v.getHora());
-			escolha = sc.nextLine();
+			escolha = JOptionPane.showInputDialog(null, msg, "Gerar Relatorio de Voo", JOptionPane.PLAIN_MESSAGE);
 			if(escolha.contentEquals("S")) {
-				sc.close();
 				return;
 			}
 				
@@ -50,39 +50,39 @@ public class Atendente extends Usuario {
 		Voo v = voos.get(id);
 		
 		//Incio do relatorio de voo
-		System.out.printf("------ Relatorio do Voo %02d ------\n"
-					//Informacoes do voo
-						+ "-> Informacoes do Voo%<02d <-"
-						+ "Origem: %s\n"
-						+ "Destino: %s\n"
-						+ "Data: %tA - %<td de %<tB de %<tY\n"
-						+ "HorÃ¡rio: %tH:%<tM\n"
-						+ "Numero de passageiros a bordo: %d\n"
-						+ "Poltronas Livres: %d\n"
-						+ "Poltronas Economicas: %d\n"
-						+ "Poltronas 1ª classe: %d\n"
-						+ "-----------------------------------\n"
-						, voos.indexOf(v)
-						, v.getOrigem()
-						, v.getDestino()
-						, v.getData()
-						, v.getHora()
-						, v.getQtdPassageirosABordo()
-						, v.getPoltronasLivres().size()
-						, v.getPoltronasLivresEconomica().size()
-						, v.getPoltronasLivresPrimeiraClasse().size());
+		msg = String.format(//Informacoes do voo
+							  "+Informacoes do Voo %02d: \n"
+							+ "+--Origem: %s\n"
+							+ "+--Destino: %s\n"
+							+ "+--Data: %tA - %<td de %<tB de %<tY\n"
+							+ "+--HorÃ¡rio: %tH:%<tM\n"
+							+ "\n"
+							, voos.indexOf(v)
+							, v.getOrigem()
+							, v.getDestino()
+							, v.getData()
+							, v.getHora());
 					//Informacoes de passageiros
-		System.out.printf("-> Informacoes dos Passageiros do Voo %02d <-\n", voos.indexOf(v));
+		msg += String.format("+Informacoes dos Passageiros do Voo %02d:\n", voos.indexOf(v));
 		
 		for (Poltrona p : v.getPoltronas()) {
 			if (p.getUsuario() != null) {
-				System.out.printf("Poltrona %02d: %s\n", v.getPoltronas().indexOf(p) + 1, p.getUsuario());
+				msg += String.format("+--Poltrona %02d: %s\n", v.getPoltronas().indexOf(p) + 1, p.getUsuario());
 			}
 		}
 			
-		System.out.printf("Numero de passageiros menores: %d\n", v.getPoltronas().stream().filter(p -> p.getUsuario() != null && p.getUsuario().getIdade() < 18).collect(Collectors.toList()).size());
-		System.out.printf("--- Fim do Relatorio do Voo %02d --\n", voos.indexOf(v));
-		sc.close();
+		msg += String.format( "+--Numero de passageiros a bordo: %d\n"
+							+ "+--Poltronas Livres: %d\n"
+							+ "+--Poltronas Economicas: %d\n"
+							+ "+--Poltronas 1a classe: %d\n"
+							+ "+--Numero de passageiros menores: %d\n"
+							, v.getQtdPassageirosABordo()
+							, v.getPoltronasLivres().size()
+							, v.getPoltronasLivresEconomica().size()
+							, v.getPoltronasLivresPrimeiraClasse().size()
+							, v.getPoltronas().stream().filter(p -> p.getUsuario() != null && p.getUsuario().getIdade() < 18).collect(Collectors.toList()).size());
+		msg += String.format("\nFim do Relatorio do Voo %02d\n", voos.indexOf(v));
+		JOptionPane.showMessageDialog(null, msg, String.format("Relatorio do Voo %02d", voos.indexOf(v)), JOptionPane.PLAIN_MESSAGE);
 	}
 
 	public void criaVoo(ArrayList<Aviao> avioes) {
