1e9a3cdd13d5191b7d5bc4cea0730b949246ba11
gianc <gianc@DESKTOP-VLVNB2H>
2019-08-01 21:48:30 -0300

    reserva feita

--- companhia-aerea/src/usuario/Usuario.java
+++ companhia-aerea/src/usuario/Usuario.java
@@ -1,7 +1,9 @@
 package usuario;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
+import aviao.Poltrona;
 import aviao.Voo;
 
 public abstract class Usuario implements Serializable {
@@ -9,10 +11,12 @@ public abstract class Usuario implements Serializable {
 	private static final long serialVersionUID = -343373518797799409L;
 	private String nome;
 	private int idade;
+	private ArrayList<Poltrona> reservadas;
 
 	public Usuario(String nome, int idade) {
 		setNome(nome);
 		setIdade(idade);
+		this.reservadas = new ArrayList<Poltrona>();
 	}
 	
 	public Usuario(String nome) {
@@ -42,10 +46,17 @@ public abstract class Usuario implements Serializable {
 		return false;
 	}
 	
-	public void reservaVoo(Voo voo) {
-		
+	public void reservaVoo(Poltrona poltrona) {
+		this.reservadas.add(poltrona);
 	}
 	
+	public void cancelaReserva(Poltrona poltrona) {
+		this.reservadas.remove(poltrona);
+	}
+	
+	public void imprimeReservas() {
+		this.reservadas.stream().forEach(c->System.out.println(c));
+	}
 	
 	public void embarca(Voo voo) {
 		
